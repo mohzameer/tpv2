ed31eb54c61962ce96ce9a140dc8b386097414ad
Mohammed Zameer <zameermfm@gmail.com>
2025-12-07 11:45:29 +0530

    Project-init

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { Button } from '@mantine/core'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        <Button 
+          variant="filled" 
+          color="blue" 
+          size="md"
+          style={{ marginTop: '1rem' }}
+        >
+          Mantine Button
+        </Button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
+
